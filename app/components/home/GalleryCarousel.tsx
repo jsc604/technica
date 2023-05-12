f6e8f1d35bbf0e37928b57911bd801a60c5a2795
Jeffrey Cheung <jeffreycheung_@live.com>
2023-05-11 21:25:21 -0700

    set up contact page, delete some photos

--- app/components/home/GalleryCarousel.tsx
+++ app/components/home/GalleryCarousel.tsx
@@ -22,12 +22,10 @@ import skylineSide from '../../../assets/images/skyline-side.jpeg';
 import ram from '../../../assets/images/ram badge 2.jpeg';
 import bentleyBack from '../../../assets/images/bentley back.jpg';
 import skylineFront from '../../../assets/images/skyline front.jpeg';
-import camaro from '../../../assets/images/camaro.jpeg';
 import camaro2 from '../../../assets/images/camaro 2.jpeg';
-import q5 from '../../../assets/images/audi q5.jpg';
 import bentley from '../../../assets/images/bentley3.png';
 
-const images = [bentley, bentleyBack, corvette, cayenneIntRear, cayenneTrunk, teslaY, benzFront, benzWheel, biturbo, lexusFront, lexus, q5, skylineFront, skylineSide, camaro, camaro2, ram];
+const images = [bentley, bentleyBack, corvette, cayenneIntRear, teslaY, benzFront, benzWheel, biturbo, lexusFront, lexus, skylineFront, skylineSide, camaro2, ram];
 
 const GalleryCarousel = () => {
 
---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div className="mt-8">
+      <h1 className="font-semibold text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-4 text-center">
+        We&apos;d Love to Hear From You!
+      </h1>
+    </div>
+  )
+};
+
+export default Contact;
