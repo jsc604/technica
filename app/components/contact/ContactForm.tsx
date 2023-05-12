fba3a0dbcfaf4fe076e459c49c00d8cf669f3042
Jeffrey Cheung <jeffreycheung_@live.com>
2023-05-12 15:55:06 -0700

    connect contact form to back end

--- app/components/contact/ContactForm.tsx
+++ app/components/contact/ContactForm.tsx
@@ -1,49 +1,103 @@
+'use client'
+import { useState } from "react";
+import { FormEvent } from "react";
+
+const apiKey = process.env.NEXT_PUBLIC_ORBIS_API_KEY;
+
 const ContactForm = () => {
+  const [contact, setContact] = useState<string>('');
+  const [phone, setPhone] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [notes, setNotes] = useState<string>('');
+  const [submitted, setSubmitted] = useState<boolean>(false);
+
+  const submitForm = async (event: FormEvent) => {
+    event.preventDefault();
+
+    let url = `
+    https://orbisx.ca/app/api/VSV93?key=${apiKey}&dothis=saveLead&source=Website&contact=${contact}&email=${email}&notes=${notes}
+    `;
+
+    if (phone) {
+      url += `&phone=${phone}`;
+    }
+
+    const response = await fetch(url, {
+      method: 'GET',
+    });
+
+    if (response.ok) {
+      setSubmitted(true);
+    } else {
+      console.error('Form submission failed');
+    }
+  };
+
   return (
     <div className="w-full my-10">
-      <div className="text-center text-xl font-semibold">How can we help?</div>
-      <form action="/send-data-here" method="get" className="flex flex-col w-10/12 sm:w-3/4 lg:w-1/2 mx-auto">
+      {!submitted && (
+        <>
+          <div className="text-center text-xl font-semibold">How can we help?</div>
+          <form onSubmit={submitForm} className="flex flex-col w-10/12 sm:w-3/4 lg:w-1/2 mx-auto">
 
-        <div className="grid sm:grid-cols-2 sm:gap-8">
-          <label htmlFor="contact" className="sr-only">Contact</label>
-          <input
-            type="text"
-            id="contact"
-            name="contact"
-            required
-            placeholder='Name*'
-            className="text-black my-4 h-10 rounded-md p-4" />
+            <div className="grid sm:grid-cols-2 sm:gap-8">
+              <label htmlFor="contact" className="sr-only">Contact</label>
+              <input
+                type="text"
+                id="contact"
+                name="contact"
+                required
+                placeholder='Name*'
+                className="text-black my-4 h-10 rounded-md p-4"
+                value={contact}
+                onChange={e => setContact(e.target.value)}
+              />
 
-          <label htmlFor="phone" className="sr-only">Phone</label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            placeholder='Phone Number'
-            className="text-black my-4 h-10 rounded-md p-4" />
-        </div>
+              <label htmlFor="phone" className="sr-only">Phone</label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                placeholder='Phone Number'
+                className="text-black my-4 h-10 rounded-md p-4"
+                value={phone}
+                onChange={e => setPhone(e.target.value)}
+              />
+            </div>
 
-        <label htmlFor="email" className="sr-only">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          required
-          placeholder='Email*'
-          className="text-black my-4 h-10 rounded-md p-4" />
+            <label htmlFor="email" className="sr-only">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              placeholder='Email*'
+              className="text-black my-4 h-10 rounded-md p-4"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
 
-        <label htmlFor="notes" className="sr-only">Message</label>
-        <textarea
-          id="notes"
-          name="notes"
-          required
-          placeholder="Message*"
-          className="text-black my-4 h-10 rounded-md p-4 h-28" />
+            <label htmlFor="notes" className="sr-only">Message</label>
+            <textarea
+              id="notes"
+              name="notes"
+              required
+              placeholder="Message*"
+              className="text-black my-4 h-10 rounded-md p-4 h-28"
+              value={notes}
+              onChange={e => setNotes(e.target.value)}
+            />
 
-        <button type="submit" className="bg-blue-500 w-fit mx-auto py-1.5 px-3.5 rounded-full">
-          Send Message
-        </button>
-      </form>
+            <button type="submit" className="bg-blue-500 w-fit mx-auto py-1.5 px-3.5 rounded-full">
+              Send Message
+            </button>
+          </form>
+        </>
+      )}
+
+      <div className="flex flex-col w-10/12 sm:w-3/4 lg:w-1/2 mx-auto">
+        
+      </div>
     </div>
   )
 }
