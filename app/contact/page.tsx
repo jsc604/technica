06ff56e1af049989609de026d7800ee188b0a0fb
Jeffrey Cheung <jeffreycheung_@live.com>
2023-05-12 17:14:46 -0700

    fixed server render error

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,18 +1,12 @@
-'use client'
-
-import { useState } from 'react';
 import TopContactWrapper from '../components/contact/TopContactWrapper';
 import ContactForm from '../components/contact/ContactForm';
-import ConfirmRequest from '../components/contact/ConfirmRequest';
 
 const Contact = () => {
-  const [submitted, setSubmitted] = useState<boolean>(true);
 
   return (
     <div className="mt-8">
       <TopContactWrapper />
-      {!submitted && <ContactForm setSubmitted={setSubmitted} />}
-      {submitted && <ConfirmRequest />}
+      <ContactForm />
     </div>
   )
 };
